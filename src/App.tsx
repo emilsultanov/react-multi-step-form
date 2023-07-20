4dc2866ba38ac1810e01c8e72e02a7a6d287aacf
emilsultanov <emilsultanov01@gmail.com>
2023-07-20 20:19:19 +0400

    Integrate yup with react-hook-form to validate forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Box} from "@mui/material";
 import {MultiStepForm, StepProps} from "./components/MultiStepForm";
+import {FormProvider, useForm} from "react-hook-form";
+
+import * as yup from 'yup'
 
 
 const initialData = {
@@ -11,6 +14,21 @@ const initialData = {
   username: '',
   experience: ''
 }
+
+const step_1_schema = yup.object({
+  firstName: yup.string().required('include firstname'),
+  lastname: yup.string().required('include lastname')
+});
+
+const step_2_schema = yup.object({
+  age: yup.number().required('include age'),
+  experience: yup.number().required('include experience')
+});
+
+const step_3_schema = yup.object({
+  email: yup.string().required('include email'),
+  username: yup.string().required('include username')
+});
 
 const Step1 = (props: any) => {
   return <div>Step-1</div>
@@ -27,15 +45,18 @@ const Step3 = (props: any) => {
 const steps: Array<StepProps> = [
   {
     label: 'Step-1',
-    step: Step1
+    step: Step1,
+    validationSchema: step_1_schema
   },
   {
     label: 'Step-2',
-    step: Step2
+    step: Step2,
+    validationSchema: step_2_schema
   },
   {
     label: 'Step-3',
-    step: Step3
+    step: Step3,
+    validationSchema: step_3_schema
   },
 ]
 
@@ -46,10 +67,10 @@ const steps: Array<StepProps> = [
 function App() {
   return (
     <Box className="app">
-      <MultiStepForm
-        steps={steps}
-        initialData={initialData}
-      />
+        <MultiStepForm
+          steps={steps}
+          initialData={initialData}
+        />
     </Box>
   );
 }
--- src/components/MultiStepForm.tsx
+++ src/components/MultiStepForm.tsx
@@ -1,30 +1,49 @@
 import {Box} from "@mui/material";
-import React, {ElementType, ReactComponentElement, ReactElement, ReactNode, useState} from "react";
+import React, {ElementType, useState} from "react";
+import {FormProvider, useForm} from "react-hook-form";
+import * as yup from 'yup'
+import {OptionalObjectSchema} from "yup/es/object";
+import {yupResolver} from "@hookform/resolvers/yup";
 
 export interface StepProps {
   label: string;
-  step: ElementType
+  step: ElementType;
+  validationSchema: any
 }
 
 export interface MultiStepFormProps<S, D> {
   steps: Array<S>,
-  initialData: D
+  defaultValues: D
 }
 
 
-export function MultiStepForm<S extends StepProps, D extends {}>({steps, initialData}: MultiStepFormProps<S, D>){
+export function MultiStepForm<S extends StepProps, D extends {}>({steps, defaultValues}: MultiStepFormProps<S, D>) {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  
+  const methods = useForm({
+	 defaultValues,
+	 mode: 'onSubmit',
+	 resolver: yupResolver(steps[currentStepIndex].validationSchema)
+  });
   
   const CurrentStep = steps[currentStepIndex].step;
   
-  return(
-	 <Box>
-		<Box>
-		  <Box width={'300px'}>{steps?.map(step => <p key={step.label}>{step?.label}</p>)}</Box>
-		  <Box width={'700px'}>
-			 <CurrentStep />
+  return (
+	 <FormProvider {...methods}>
+		<form>
+		  <Box>
+			 <Box display={'flex'}>
+				<Box width={'300px'}>{steps?.map((step) => <p key={step.label}>{step?.label}</p>)}</Box>
+				<Box width={'700px'}>
+				  <CurrentStep/>
+				</Box>
+			 </Box>
+			 <Box>
+				<button>prev</button>
+				<button>next</button>
+			 </Box>
 		  </Box>
-		</Box>
-	 </Box>
+		</form>
+	 </FormProvider>
   )
 }
