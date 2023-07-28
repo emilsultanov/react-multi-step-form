e4aa1022021eb21870d81b54e6b9a923865d6ccd
emilsultanov <emilsultanov01@gmail.com>
2023-07-29 00:28:19 +0400

    Create multi-steo form layout from scratch

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,37 @@
 import React from 'react';
-import {Box} from "@mui/material";
-import {MultiStepForm, StepProps} from "./components/MultiStepForm";
-import {FormProvider, useForm} from "react-hook-form";
-
-import * as yup from 'yup'
-
-
-const initialData = {
-  firstName: '',
-  lastName: '',
-  age: '',
-  email: '',
-  username: '',
-  experience: ''
-}
-
-const step_1_schema = yup.object({
-  firstName: yup.string().required('include firstname'),
-  lastname: yup.string().required('include lastname')
-});
-
-const step_2_schema = yup.object({
-  age: yup.number().required('include age'),
-  experience: yup.number().required('include experience')
-});
-
-const step_3_schema = yup.object({
-  email: yup.string().required('include email'),
-  username: yup.string().required('include username')
-});
-
-const Step1 = (props: any) => {
-  return <div>Step-1</div>
-};
-
-const Step2 = (props: any) => {
-  return <div>Step-2</div>
-};
-
-const Step3 = (props: any) => {
-  return <div>Step-3</div>
-};
-
-const steps: Array<StepProps> = [
-  {
-    label: 'Step-1',
-    step: Step1,
-    validationSchema: step_1_schema
-  },
-  {
-    label: 'Step-2',
-    step: Step2,
-    validationSchema: step_2_schema
-  },
-  {
-    label: 'Step-3',
-    step: Step3,
-    validationSchema: step_3_schema
-  },
-]
-
-
+import {Box, Container, Grid, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
 
 
 
 function App() {
   return (
     <Box className="app">
-        <MultiStepForm
-          steps={steps}
-          initialData={initialData}
-        />
+      <Container sx={{paddingY: '50px'}}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Step-1" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Step-2" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary="Step-3" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={8}>
+            right
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   );
 }
--- src/components/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IStep {
-  id: number | string,
-  label: string;
-  stepForm: () => JSX.Element
-}
-
